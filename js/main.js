0408bd62d02260eb2585bd62a3d8cbe7ab429523
Ryan Purse <ryanpurse@gmail.com>
2020-10-17 15:50:44 +0100

    Attempted to add stand alone header and footer

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // Created By: Ryan Purse
 // Created On: 15/10/2020
 
-
+includeHTML();
 setPseudoHeaderHeight();
 
 function setPseudoHeaderHeight() {
@@ -11,4 +11,20 @@ function setPseudoHeaderHeight() {
         so that it can off set the page
      */
     document.getElementById("pseudoHeader").style.height = document.getElementById("header").offsetHeight.toString() + "px"
+}
+
+function includeHTML() {
+    /* Find elements with a specific tag */
+    const tag_name = "include";
+    const atr_name = "src"; /* attribute tag is will try and look for */
+    let includes = document.getElementsByTagName(tag_name);
+    for (const include of includes) {
+        let include_file_name = include.getAttribute(atr_name);
+        if (include_file_name) {
+            let xhttp = new XMLHttpRequest();
+            xhttp.open("GET", include_file_name, true);
+            xhttp.send();
+        }
+        else (console.log("File not found"));
+    }
 }
